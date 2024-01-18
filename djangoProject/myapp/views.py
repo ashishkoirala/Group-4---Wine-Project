cd163ccfb25c456bc14c8f38753c4ec85844bf86
ASHISH KOIRALA <ashish.koirala3@gmail.com>
2024-01-18 11:03:04 -0500

    Initial Run

--- djangoProject/myapp/views.py
+++ djangoProject/myapp/views.py
@@ -1,3 +1,11 @@
+
+from datetime import datetime
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    cur_time = datetime.now()
+    context = {}
+    context['time'] = cur_time
+    return render(request, 'myapp/homepage.html', context=context)
