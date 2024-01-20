bd44c6af00a3b4f2057ae831ea8d7c1d723ab7f5
vivekn <vnautiyal24@gsb.columbia.edu>
2024-01-20 13:17:23 -0500

    Add trending feature pre merge

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'trends',
 ]
 
 MIDDLEWARE = [
--- djangoProject/myapp/views.py
+++ djangoProject/myapp/views.py
@@ -8,14 +8,8 @@ from .forms import WinePreferenceForm
 from .models import Wine
 import csv
 from io import TextIOWrapper
-
-
-# Create your views here.
-def home(request):
-    cur_time = datetime.now()
-    context = {}
-    context['time'] = cur_time
-    return render(request, 'myapp/homepage.html', context=context)
+from pytrends.request import TrendReq
+from django.shortcuts import render
 
 # View for Model 1:To upload the .csv file scrapped from 'https://www.danmurphys.com.au/list/wine?filters=country(italy)'
 
@@ -104,3 +98,29 @@ def wine_recommendation(request):
 
     else:
         return wine_preferences(request)
+
+def trending_view(request):
+    selected_region = request.GET.get('region')  # Get the selected region from the request
+
+    pytrends = TrendReq(hl='en-US', tz=360)
+    kw_list = ["Cabernet Sauvignon", "Pinot Noir", "Chardonnay", "Merlot", "Sauvignon Blanc"]
+
+    # Check if a region is selected, otherwise default to 'US'
+    if not selected_region:
+        selected_region = 'US'
+
+    pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo=selected_region, gprop='')
+    interest_over_time_df = pytrends.interest_over_time()
+
+    # Dropping 'isPartial' column
+    if 'isPartial' in interest_over_time_df.columns:
+        interest_over_time_df = interest_over_time_df.drop('isPartial', axis=1)
+
+    # Sum up and sort values to get top 3
+    trending_wines = interest_over_time_df.sum().sort_values(ascending=False).head(3)
+
+    context = {
+        'selected_region': selected_region,
+        'trending_wines': trending_wines.to_dict(),
+    }
+    return render(request, 'myapp/trending.html', context)
